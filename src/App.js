5fb8f8847eaaae15e0c6640239014fca6e27583c
PhungThinhVuong <phungthinhvuong2101@gmail.com>
2022-09-23 23:12:18 +0700

    done

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Home from "./Components/BaiTapThucHanhLayOut/Home";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Home/>
+    </div>
+  );
+}
